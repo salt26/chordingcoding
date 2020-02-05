a3833f7381de19b96656b2b5eeebb9540b3f3bc6
Dantae An <48adt@naver.com>
2020-02-05 18:40:20 +0900

    add accomp patterns to Autumn theme, minor bug fixed

--- ChordingCoding/Theme.cs
+++ ChordingCoding/Theme.cs
@@ -242,7 +242,7 @@ namespace ChordingCoding
             */
 
             _availableThemes.Add(new Theme(
-                "Medieval ruins", "중세 유적지",
+                "Medieval_ruins", "중세 유적지",
                 new ParticleSystem(1, 0,
                                    ParticleSystem.CreateFunction.TopRandom,
                                    Particle.Type.rain, () => Color.White,
@@ -255,7 +255,7 @@ namespace ChordingCoding
                                    Particle.Type.rain, () => Color.White,
                                    0.1f, (Form1.form1.Size.Height + 150) / 30),
                 new Theme.ParticleInfo(Particle.Type.note, (Form1.form1.Size.Height + 150) / 15, (pitch) => Chord.PitchColor(pitch), 0.1f),
-                "Medieval ruins"));
+                "Medieval_ruins"));
 
             IsReady = true;
         }

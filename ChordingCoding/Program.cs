b91616c30d38b760a7d82177fe38a274e0cf0db5
안단태 <48adt@naver.com>
2022-06-14 15:48:10 +0900

    update licenses

--- ChordingCoding/Program.cs
+++ ChordingCoding/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*
 MIT License
 
-Copyright (c) 2019 salt26
+Copyright (c) 2019 Dantae An
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
